5cf056c09f3865ac93534dc6c6ed2156ba89f29d
Todd Whitelow <todd@whitelow.tech>
2021-06-17 22:54:50 -0600

    took away subdomain and left netlify domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,15 +8,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8080'
-
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
   allow do
-    origins 'affectionate-nobel-beb889.netlify.app'
-
+    origins 'netlify.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
